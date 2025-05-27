bcfc741dc4b602176b8e03b5a2f93308b78e8f61
TONG Wai Fung (240708635) <240708635@stu.vtc.edu.hk>
2025-05-27 12:43:36 +0800

    first commit

---
+++ Sunshine/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Sunshine
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.forgetPasswordLL = new System.Windows.Forms.LinkLabel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.passwordPB = new System.Windows.Forms.PictureBox();
+            this.usernamePB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usernamePB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.Location = new System.Drawing.Point(276, 229);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.Size = new System.Drawing.Size(276, 20);
+            this.passwordTB.TabIndex = 3;
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.Location = new System.Drawing.Point(276, 165);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(276, 20);
+            this.usernameTB.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(218, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Login";
+            // 
+            // forgetPasswordLL
+            // 
+            this.forgetPasswordLL.AutoSize = true;
+            this.forgetPasswordLL.BackColor = System.Drawing.Color.Transparent;
+            this.forgetPasswordLL.Location = new System.Drawing.Point(221, 273);
+            this.forgetPasswordLL.Name = "forgetPasswordLL";
+            this.forgetPasswordLL.Size = new System.Drawing.Size(101, 13);
+            this.forgetPasswordLL.TabIndex = 6;
+            this.forgetPasswordLL.TabStop = true;
+            this.forgetPasswordLL.Text = "Forgotten Password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.SpringGreen;
+            this.loginButton.Location = new System.Drawing.Point(221, 304);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(331, 36);
+            this.loginButton.TabIndex = 7;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // passwordPB
+            // 
+            this.passwordPB.BackColor = System.Drawing.Color.Transparent;
+            this.passwordPB.Image = global::Sunshine.Properties.Resources.lock_24dp;
+            this.passwordPB.InitialImage = global::Sunshine.Properties.Resources.person_24dp;
+            this.passwordPB.Location = new System.Drawing.Point(221, 229);
+            this.passwordPB.Name = "passwordPB";
+            this.passwordPB.Size = new System.Drawing.Size(24, 24);
+            this.passwordPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.passwordPB.TabIndex = 2;
+            this.passwordPB.TabStop = false;
+            // 
+            // usernamePB
+            // 
+            this.usernamePB.BackColor = System.Drawing.Color.Transparent;
+            this.usernamePB.Image = global::Sunshine.Properties.Resources.person_24dp;
+            this.usernamePB.InitialImage = global::Sunshine.Properties.Resources.person_24dp;
+            this.usernamePB.Location = new System.Drawing.Point(221, 165);
+            this.usernamePB.Name = "usernamePB";
+            this.usernamePB.Size = new System.Drawing.Size(24, 24);
+            this.usernamePB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.usernamePB.TabIndex = 1;
+            this.usernamePB.TabStop = false;
+            // 
+            // LoginPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.forgetPasswordLL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usernameTB);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.passwordPB);
+            this.Controls.Add(this.usernamePB);
+            this.Name = "LoginPage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Page";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.passwordPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usernamePB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox usernamePB;
+        private System.Windows.Forms.PictureBox passwordPB;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel forgetPasswordLL;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ Sunshine/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Sunshine
+{
+    public partial class LoginPage : Form
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            // Create a linear gradient from top-left to bottom-right
+            using (LinearGradientBrush brush = new LinearGradientBrush(
+                this.ClientRectangle,
+                Color.LightSkyBlue, // Start color
+                Color.LightYellow,  // End color
+                LinearGradientMode.ForwardDiagonal))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
